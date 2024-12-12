7c74ebc629a6533c2f152b5e9773f6d1c782ca03
Mohamed0005 <mohamedyoussef47M@gmail.com>
2024-12-13 00:54:05 +0200

    Update Grade.java

--- src/Grade.java
+++ src/Grade.java
@@ -1,2 +1,30 @@
 public class Grade {
+    private Student student;
+    private Courses course;
+    private double grade;
+
+    public Grade(Student student, Courses course, double grade) {
+        this.student = student;
+        this.course = course;
+        this.grade = grade;
+    }
+    public Student getStudent() {
+        return student;
+    }
+    public Courses getCourse() {
+        return course;
+    }
+    public double getGrade() {
+        return grade;
+    }
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+    public static void recordGrade(Student student, Courses course, double grade) {
+        Grade newGrade = new Grade(student, course, grade);
+        student.addGrade(course.getCourse_name(), newGrade);
+    }
+    public String toString() {
+        return "Grade for " + student.getName() + " in " + course.getCourse_name() + ": " + grade;
+    }
 }
